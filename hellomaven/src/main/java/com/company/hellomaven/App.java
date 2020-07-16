c5885a4dff948eab77a2290af3b47574ca7e47f5
Robert Wang <rwang5688@outlook.com>
2020-07-15 23:00:54 -0700

    add VS code maven quickstart

---
+++ hellomaven/src/main/java/com/company/hellomaven/App.java
@@ -0,0 +1,17 @@
+package com.company.hellomaven;
+
+/**
+ * Hello Maven!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Maven!");
+    }
+}
