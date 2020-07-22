4642a08581ede0899961599a8c06b282ace5fb29
Robert Wang <rwang5688@outlook.com>
2020-07-22 01:47:48 -0700

    add is valid BST check

--- trees/src/main/java/com/companyname/trees/TreeUtils.java
+++ trees/src/main/java/com/companyname/trees/TreeUtils.java
@@ -21,7 +21,7 @@ public final class TreeUtils {
     }
 
     /**
-     * findMaxDepth - recursive method for finding max depth.
+     * findMaxDepth - recursive private method for finding max depth.
      * @param currentNode
      * @param currentDepth
      * @return maximum tree depth
@@ -44,5 +44,54 @@ public final class TreeUtils {
 
         // compare and return bigger value
         return (leftMaxDepth > rightMaxDepth) ? leftMaxDepth : rightMaxDepth;
+    }
+
+    /**
+     * isValidBST - check if tree is a strict binary search tree.
+     * @param root
+     * @return false if any value is dup or values are not sorted as a strict BST; otherwise true
+     */
+    public static boolean isValidBST(TreeNode root) {
+        // if any node returns false, then whole tree is false
+        return isValidBSTWithinRange(root, null, null);
+    }
+
+    /**
+     * isValidBSTWithinRange - recursive private method for checking sub-tree is within range.
+     * @param root
+     * @param lower
+     * @param upper
+     * @return
+     */
+    private static boolean isValidBSTWithinRange(TreeNode root, Integer lower, Integer upper) {
+        // empty tree is always valid
+        if (root == null) {
+            return true;
+        }
+
+        // am I within range?
+        if ((lower != null) && (root.getVal() <= lower)) {
+            return false;
+        }
+        if ((upper != null) && (root.getVal() >= upper)) {
+            return false;
+        }
+
+        // check my left sub-tree
+        if (root.getLeft() != null) {
+            if (!isValidBSTWithinRange(root.getLeft(), lower, root.getVal())) {
+                return false;
+            }
+        }
+
+        // check my right sub-tree
+        if (root.getRight() != null) {
+            if (!isValidBSTWithinRange(root.getRight(), root.getVal(), upper)) {
+                return false;
+            }
+        }
+
+        // sub-trees are valid
+        return true;
     }
 }
