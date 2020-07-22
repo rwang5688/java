9f9c3ae2e1dec0b928e25371606ec4c6428e5803
Robert Wang <rwang5688@outlook.com>
2020-07-21 18:56:01 -0700

    add exercise1 boilerplate

---
+++ exercise1/src/main/java/com/companyname/exercise1/App.java
@@ -0,0 +1,17 @@
+package com.companyname.exercise1;
+
+/**
+ * Hello Exercise 1!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to Exercise 1.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Exercise 1!");
+    }
+}
